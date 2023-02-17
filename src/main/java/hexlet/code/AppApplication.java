04e4f33a1b38e39b49204fa41c417b043921f119
Maria Teplinskaya <pythagoreantree@googlemail.com>
2023-02-18 01:42:20 +0300

    current

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -2,8 +2,6 @@ package hexlet.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AppApplication {
