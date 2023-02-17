b57c48e960e266f5df0d408ce3b7c6aa8dc64769
Maria Teplinskaya <pythagoreantree@googlemail.com>
2023-02-18 02:35:20 +0300

    add User

---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,50 @@
+package hexlet.code.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+import static jakarta.persistence.GenerationType.AUTO;
+import static jakarta.persistence.TemporalType.TIMESTAMP;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    @JsonIgnore
+    private String password;
+
+    @CreationTimestamp
+    @Temporal(TIMESTAMP)
+    private Date createdAt;
+
+    public User(final Long id) {
+        this.id = id;
+    }
+}
