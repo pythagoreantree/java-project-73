0e284d6f429fe553a469eeb679e3ac44f3a3b705
Maria Teplinskaya <pythagoreantree@googlemail.com>
2023-03-12 21:49:11 +0300

    fix checkstyle

--- src/test/java/hexlet/code/config/SpringConfigForIT.java
+++ src/test/java/hexlet/code/config/SpringConfigForIT.java
@@ -4,7 +4,6 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
