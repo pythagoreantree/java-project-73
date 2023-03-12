70c13c8c8a7863326631751df0fe32b51363594d
Maria Teplinskaya <pythagoreantree@googlemail.com>
2023-03-12 23:01:45 +0300

    fix checkstyle

--- src/main/java/hexlet/code/controllers/UserController.java
+++ src/main/java/hexlet/code/controllers/UserController.java
@@ -12,8 +12,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
